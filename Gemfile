f88bc6e41b0da6625c4c4e1e3c855e706565c5b6
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-05 01:15:56 +0300

    fix linting

--- Gemfile
+++ Gemfile
@@ -1,14 +1,16 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
-gem "minitest", "~> 5.25"
-gem "minitest-power_assert", "~> 0.3.1"
+source 'https://rubygems.org'
+
+gem 'minitest', '~> 5.25'
+gem 'minitest-power_assert', '~> 0.3.1'
 
 group :test do
-  gem "simplecov", "~> 0.22", require: false
-  gem "simplecov-lcov", "~> 0.8.0", require: false
+  gem 'simplecov', '~> 0.22', require: false
+  gem 'simplecov-lcov', '~> 0.8.0', require: false
 end
 
 group :development, :test do
-  gem "rubocop", "1.50.2", require: false
-  gem "rubocop-minitest", "0.28.0", require: false
+  gem 'rubocop', '1.50.2', require: false
+  gem 'rubocop-minitest', '0.28.0', require: false
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "simplecov"
+require 'simplecov'
 
-if ENV["CI"]
-  require "simplecov-lcov"
+if ENV['CI']
+  require 'simplecov-lcov'
 
   SimpleCov::Formatter::LcovFormatter.config do |c|
     c.report_with_single_file = true
-    c.single_report_path = "coverage/lcov.info"
+    c.single_report_path = 'coverage/lcov.info'
   end
 
   SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
@@ -17,9 +17,9 @@ end
 
 SimpleCov.start do
   enable_coverage :branch
-  add_filter "/test/"
+  add_filter '/test/'
   minimum_coverage 90
 end
 
-require "minitest/autorun"
-require "minitest/power_assert"
+require 'minitest/autorun'
+require 'minitest/power_assert'
